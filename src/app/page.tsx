f1aa5b554185e2f40e031d6680333c71324dc10e
u1810291 <otabekbutcher@gmail.com>
2024-08-12 16:10:31 +0500

    feat: welcome page is ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-import Image from 'next/image'
-import { useState } from 'react'
-
 export default function Home() {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [showPassword, setShowPassword] = useState<string>('password')
-
   return (
     <main className='h-screen flex'>
-      <h1>Welcome to the application</h1>
+      <div className='w-screen h-screen justify-center content-center'>
+        <h1 className='text-4xl text-center'>Welcome to the application</h1>
+      </div>
     </main>
-  );
+  )
 }
